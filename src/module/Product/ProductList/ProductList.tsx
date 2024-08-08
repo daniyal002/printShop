4c496b7b3b58a820c932d4e2e2d7233b6cd388b7
daniyal002 <daniyalou@gmail.com>
2024-08-08 10:36:40 +0300

    update

--- src/module/Product/ProductList/ProductList.tsx
+++ src/module/Product/ProductList/ProductList.tsx
@@ -22,7 +22,7 @@ export const ProductList: React.FC = () => {
   // Инициируем searchProduct как пустой массив
   const [searchProduct, setSearchProduct] = useState<IProduct[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const itemsPerPage = 10;
 
   useEffect(() => {
     if (isSuccess) {
