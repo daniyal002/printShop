9786bef1ad4ab78b425d3ee59f6718e169062d7c
daniyal002 <daniyalou@gmail.com>
2024-09-03 16:13:27 +0300

    update

--- src/api/interseptots.ts
+++ src/api/interseptots.ts
@@ -1,13 +1,11 @@
 import axios, { CreateAxiosDefaults } from "axios"
 
 // export const baseURL = "http://81.200.147.55:8000"
-// export const baseURL = "https://api.fabrika-kruzhek.ru"
-export const baseURL = "http://localhost:3030"
+export const baseURL = "https://api.fabrika-kruzhek.ru"
 export const tel = "79280484105"
 
 const options:CreateAxiosDefaults = {
     baseURL:baseURL,
-    // baseURL:"http://localhost:3030",
     headers:{
         'Content-Type': 'application/json',
     },
