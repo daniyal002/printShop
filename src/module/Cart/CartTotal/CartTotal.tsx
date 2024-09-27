3047f0e7889a116512d2c467a3a32435000d827c
daniyal002 <daniyalou@gmail.com>
2024-09-27 18:28:05 +0300

    fix video

--- src/module/Cart/CartTotal/CartTotal.tsx
+++ src/module/Cart/CartTotal/CartTotal.tsx
@@ -12,16 +12,18 @@ export function CartTotal(){
         Количество: cart.count
     }));
     const totalCount = carts.reduce((sum,current) => sum + current.count,0 )
-    const message = readableCarts.map(cart => 
-         Object.values(cart).join(', ')
-     ).join('; ');
+    const message = readableCarts.map((cart, index) => {
+        const productName = cart.Товар;
+        const quantity = cart.Количество;
+        return `${productName} - ${quantity} шт${index < readableCarts.length - 1 ? ', ' : ''}`;
+      }).join('');
 
     return(
         <div className={style.totalContainer}>
             <div className={style.total}>
                 <p>Общая сумма: <span>{total()}₽</span></p>
                 <p>Количество товаров: <span>{totalCount}</span></p>
-                <a className={style.btnProductPay} href={`https://wa.me/${tel}?text=Здравствуйте%2C+хочу+купить+${message}%2C+Общая+сумма:+${total()}%2C+Количество+товаров:+${totalCount}`} target="_blank">КУПИТЬ <BadgeRussianRuble className={style.badgeRussianRuble}/></a>
+                <a className={style.btnProductPay} href={`https://wa.me/${tel}?text=Здравствуйте%2C+хочу+купить+${message}%2C+Общая+сумма:+${total()}₽%2C+Количество+товаров:+${totalCount}`} target="_blank">КУПИТЬ <BadgeRussianRuble className={style.badgeRussianRuble}/></a>
             </div>
         </div>
     )
--- src/module/Product/ProductItemById/ProductItemByIdCarusel.tsx
+++ src/module/Product/ProductItemById/ProductItemByIdCarusel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Carousel, ConfigProvider, Image } from "antd";
 import { baseURL } from "../../../api/interseptots";
 import ReactPlayer from "react-player";
@@ -14,31 +14,53 @@ const contentStyle: React.CSSProperties = {
   background: "#364d79",
 };
 
+
+
 const ProductItemByIdCarusel = ({ images, video }: Props) => {
-  console.log(images)
+  const playerRef = useRef<ReactPlayer>(null);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  const handleBeforeChange = (currentSlide: number) => {
+    // Останавливаем видео, если переходим с видео слайда
+    if (currentSlide === 0 && video) {
+      setIsPlaying(false);
+    }
+  };
+
+  const handleAfterChange = (nextSlide: number) => {
+    // Запускаем видео, если переходим на видео слайд
+    if (nextSlide === 0 && video) {
+      setIsPlaying(true);
+    }
+  };
+
   return (
     <ConfigProvider
       theme={{
         components: {
           Carousel: {
             arrowOffset: 1,
+            arrowSize:32,
           },
         },
       }}
     >
       <Carousel
-        // autoplay
         draggable
         arrows
         swipe
+        infinite={false}
         style={{ width: "100%", margin: "0 auto", textAlign: "center" }}
+        beforeChange={handleBeforeChange}
+        afterChange={handleAfterChange}
       >
-         {video && (
-          <div style={{ display: "inline", margin: "0 auto",height:"424px" }}>
+        {video && (
+          <div style={{ display: "inline", margin: "0 auto", height: "424px" }}>
             <ReactPlayer
+              ref={playerRef}
               url={`${baseURL}${video}`}
               loop
-              // playing
+              playing={isPlaying}
               controls
               width={"100%"}
               height={424}
@@ -46,14 +68,15 @@ const ProductItemByIdCarusel = ({ images, video }: Props) => {
                 textAlign: "center",
                 width: "100%",
                 margin: "10px auto",
-                height:"115%"
+                height: "115%",
               }}
+              onPause={() => setIsPlaying(false)}
+              onPlay={() => setIsPlaying(true)}
             />
           </div>
         )}
-        {images.map((image,index) => (
-          <div style={{ display: "inline", margin: "0 auto" }}  key={index}>
-
+        {images.map((image, index) => (
+          <div style={{ display: "inline", margin: "0 auto" }} key={index}>
             <Image
               width={300}
               src={`${baseURL}/uploads/${image}`}
