84ed3841bfe0bdaf6064314d140ffa86dca855ee
daniyal002 <daniyalou@gmail.com>
2024-09-30 16:09:51 +0300

    "Refactored CartTotalModal component: updated message formatting and WhatsApp link"

--- src/module/Cart/CartTotalModal/CartTotalModal.tsx
+++ src/module/Cart/CartTotalModal/CartTotalModal.tsx
@@ -8,22 +8,24 @@ interface Props{
     isModalOpen:boolean,
     handleOk:any
     handleCancel:any
-    
+
 }
 
 export function CartTotalModal({isModalOpen,handleOk,handleCancel}:Props){
     const carts = useCartStore(state => state.carts)
     const total = useCartStore(state => state.total)
-  
+
     const readableCarts = carts.map(cart => ({
         Товар: cart.product.product_name,
         Количество: cart.count
     }));
-    
+
         const totalCount = carts.reduce((sum,current) => sum + current.count,0 )
-        const message = readableCarts.map(cart => 
-            Object.values(cart).join(', ')
-        ).join('; ');
+        const message = readableCarts.map((cart, index) => {
+            const productName = cart.Товар;
+            const quantity = cart.Количество;
+            return `${productName} - ${quantity} шт${index < readableCarts.length - 1 ? ', ' : ''}`;
+          }).join('');
     return(
         <Modal title="Итоги" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}  footer={() => (
             <>
@@ -33,7 +35,7 @@ export function CartTotalModal({isModalOpen,handleOk,handleCancel}:Props){
              <div className={style.total}>
             <p>Общая сумма: <span>{total()}</span></p>
             <p>Количество товаров: <span>{totalCount}</span></p>
-            <a className={style.btnProductPay} href={`https://wa.me/${tel}?text=Здравствуйте%2C+хочу+купить+${message}%2C+Общая+сумма:+${total()}%2C+Количество+товаров:+${totalCount}`} target="_blank">КУПИТЬ <BadgeRussianRuble className={style.badgeRussianRuble}/></a>
+            <a className={style.btnProductPay} href={`https://wa.me/${tel}?text=Здравствуйте%2C+хочу+купить+${message}%2C+Общая+сумма:+${total()}₽%2C+Количество+товаров:+${totalCount}`} target="_blank">КУПИТЬ <BadgeRussianRuble className={style.badgeRussianRuble}/></a>
         </div>
       </Modal>
     )
